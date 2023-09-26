b93bc91b5ec83e570236ba8ad03ae74b497918cb
Matt Todd <mtodd447@gmail.com>
2023-09-26 16:36:31 -0700

    Menus
    - Created prototype menu screen, main menu and inventory
    - game pauses during menu open
    - imported new gui art assets
    - renamed temp art folder
    - started relocating permanent art assets to assets/art/...

--- Assets/Scripts/Menus/UI.cs
+++ Assets/Scripts/Menus/UI.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UI : MonoBehaviour
 {
@@ -9,7 +11,16 @@ public class UI : MonoBehaviour
     //private variables
     [SerializeField] private SpriteRenderer closedChest;
     [SerializeField] private SpriteRenderer openChest;
+    [SerializeField] private Image pause;
+    [SerializeField] private GameObject menu;
+    [SerializeField] private GameObject menu_main;
+    [SerializeField] private GameObject menu_inventory;
+    [SerializeField] private TMP_Text header;
+
     private bool menuOpen;
+    private bool mainMenuOpen;
+    private bool inventoryOpen;
+
 
 
     void Start()
@@ -19,23 +30,90 @@ public class UI : MonoBehaviour
         
     void Update()
     {// Update is called once per frame
-
+        PlayerInput();
     }
 
-    public void ToggleChest()
+    private void PlayerInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {//toggle main menu
+            ToggleMenu();
+            DisplayMainMenu();
+        }
+        else if (Input.GetKeyDown(KeyCode.I) || Input.GetKeyDown(KeyCode.Tab))
+        {
+            ToggleMenu();
+            DisplayInventory();
+        }
+    }
+
+    public void ToggleMenu()
     {//toggle HUD chest icon open/closed
 
         if (menuOpen == false)
-        {//open chest
+        {//open menu
             menuOpen = true;
+            menu.SetActive(true);
+        }
+        else if (menuOpen == true)
+        {//close menu
+            menuOpen = false;
+            menu.SetActive(false);
+        }
+
+        TogglePause();
+        ToggleChest();
+        ResetMenu();
+    }
+
+    private void ToggleChest()
+    {
+        if (menuOpen == true)
+        {//toggle chest sprite
             closedChest.enabled = false;
             openChest.enabled = true;
         }
-        else if (menuOpen == true)
-        {//close chest
-            menuOpen = false;
+        else if (menuOpen == false)
+        {//toggle chest sprite
             closedChest.enabled = true;
             openChest.enabled = false;
         }
+    }
+
+    private void TogglePause()
+    {
+        if (menuOpen == true)
+        {//pause game
+            pause.enabled = true;
+            Time.timeScale = 0;
+
+        }
+        else if (menuOpen == false)
+        {//resume game
+            pause.enabled = false;
+            Time.timeScale = 1;
+        }
+    }
+
+    public void DisplayMainMenu()
+    {
+        ResetMenu();
+        header.text = "Main Menu";
+        menu_main.SetActive(true);
+
+    }
+
+    public void DisplayInventory()
+    {
+        ResetMenu();
+        header.text = "Inventory";
+        menu_inventory.SetActive(true);
+    }
+
+    private void ResetMenu()
+    {//close all sub menus
+        header.text = "";
+        menu_main.SetActive(false);
+        menu_inventory.SetActive(false);
     }
 }
